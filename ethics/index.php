156ada2226001f43fdceae8a547243e6fc58a41f
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-12 18:00:58 +0100

    now dealing with landing page

--- ethics/index.php
+++ ethics/index.php
@@ -1,44 +1,5 @@
 <?php
-    include('connection.php');
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        if(!empty($_POST['username']) && !empty($_POST['password'])) {
-            $username = $_POST['username'];
-            $pword = $_POST['password'];
-            $query2= "SELECT * FROM student WHERE student_ID ='$username' ";
-            $query3 ="SELECT * FROM staff WHERE staff_ID ='$username' ";
-               $result1=  mysqli_query($link, $query2);
-                $result2=  mysqli_query($link, $query3);
-            $row = "";
 
-                    if(mysqli_num_rows($result1)==1 ) {
-                        $row = mysqli_fetch_assoc($result1);
-                    }
-                    else if(mysqli_num_rows($result2)==1){
-                            $row = mysqli_fetch_assoc($result2);
-                    }
-                        $hash = $row['password'];
-                    if(password_verify($pword, $hash)) {
-                            session_start();
-
-
-                            $_SESSION['ufname'] = $row['firstname'];
-                            $_SESSION['ulname'] = $row['lastname'];
-                            $_SESSION['uemail'] = $row['email'];
-
-                           header('location: landing.php');
-                        }
-                        else{
-                            echo "Incorrect Password";
-                        }
-                    }
-                    else{
-                        echo "Invalid Login Please Try Again";
-                    }
-                    
-
-
-
-        }
 
 
 
@@ -69,7 +30,7 @@
                 <h2 class="hfont" align="center"> E-Review</h2>
                 <h3 align="center">Log in</h3>
 
-            <form action="index.php" method="POST">
+            <form action="verify.php" method="POST">
             <div class="form-group">
                 <input type="text" name="username" placeholder ="Username" class="form-control"/>
             </div>
---
+++ ethics/verify.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1613112
+ * Date: 12/04/2017
+ * Time: 17:57
+ */
+include('connection.php');
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(!empty($_POST['username']) && !empty($_POST['password'])) {
+        $username = $_POST['username'];
+        $pword = $_POST['password'];
+        $query2= "SELECT * FROM student WHERE student_ID ='$username' ";
+        $query3 ="SELECT * FROM staff WHERE staff_ID ='$username' ";
+        $result1=  mysqli_query($link, $query2);
+        $result2=  mysqli_query($link, $query3);
+        $row = "";
+
+        if(mysqli_num_rows($result1)==1 ) {
+            $row = mysqli_fetch_assoc($result1);
+        }
+        else if(mysqli_num_rows($result2)==1){
+            $row = mysqli_fetch_assoc($result2);
+        }
+        else{
+            header('location: index.php');
+        }
+        $hash = $row['password'];
+        if(password_verify($pword, $hash)) {
+            session_start();
+
+
+            $_SESSION['ufname'] = $row['firstname'];
+            $_SESSION['ulname'] = $row['lastname'];
+            $_SESSION['uemail'] = $row['email'];
+
+            header('location: landing.php');
+        }
+        else{
+            echo "Incorrect Password";
+            header('location: index.php');
+        }
+    }
+    else{
+        echo "Invalid Login Please Try Again";
+        header('location: index.php');
+    }
+
+
+
+
+}
