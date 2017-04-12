0d8599f30f304b5f5a5779d3b09ebb05da8d80fe
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-12 11:12:46 +0100

    LOGIN AND REGISTRATION FORMS bootstraped with my css

--- ethics/register.php
+++ ethics/register.php
@@ -18,6 +18,7 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
         <link rel="stylesheet" href="style.css" >
         <title>Login</title>
     </head>
