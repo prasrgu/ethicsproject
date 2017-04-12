8ee1527ca4cc1d6fdc1dcee866a20f69c98d3d67
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-12 10:34:13 +0100

    LOGIN AND REGISTRATION FORMS bootstraped

--- ethics/index.php
+++ ethics/index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
-
+<html>
 <head>
              <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 </head>
 <body>
         <form>
-            <input type="text" name="username" placeholder ="Username"/>
-            <input type="password" name="password" placeholder ="Password"/>
-            <button> Log in</button>
+            <div class="form-group">
+                <input type="text" name="username" placeholder ="Username" class="form-control"/>
+            </div>
+
+            <div class="form-group">
+                <input type="password" name="password" placeholder ="Password" class="form-control"/>
+            </div>
+
+            <button type="submit" class="btn btn-primary"> Log in</button>
         </form>
 
 
--- ethics/register.php
+++ ethics/register.php
@@ -8,27 +8,49 @@
 
 ?>
 <!DOCTYPE html>
-
+<html>
     <head>
         <title>Login</title>
     </head>
     <body>
                 <form>
-                    <input type="text" name="firstname" placeholder="firstname"/>
-                    <input type="text" name="lastname" placeholder="lastname"/>
-                    <input type="text" name="userid" placeholder="login ID"/>
-                    <input type="email" name="email" placeholder="Email"/>
-                    <input type="text" name="address" placeholder="Address Line 1"/>
-                    <input type="text" name="address" placeholder="Address Line 2"/>
-                    <input type="text" name="address" placeholder="Address Line 3"/>
-                    <select name="role">
+                    <div class="form-group">
+                        <input type="text" name="firstname" placeholder="firstname" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="lastname" placeholder="Lastname" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="userid" placeholder="login ID" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="email" placeholder="Email" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="address" placeholder="Address Line 1" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="address2" placeholder="Address Line 2" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="address3" placeholder="Address Line 3" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                    <select name="role" class="form-control">
                         <option value="nothing">Please select a role</option>
                         <option value = "STUDENT">Student</option>
                         <option value="EAO">EAO</option>
                         <option value="ADMIN">Admin</option>
                     </select>
-                    <input type="password" name="password" placeholder="Password"/>
-                    <button> Log in</button>
+                        </div>
+                    <div class="form-group">
+                        <input type="password" name="password" placeholder="Password" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password1" placeholder="Confirm Password" class="form-control"/>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary"> Sign up</button>
                 </form>
 
 
