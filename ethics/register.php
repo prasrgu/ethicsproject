bdeb540278a3c9dcb8ae64e4e2bc6fc49709af24
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-12 10:09:50 +0100

    LOGIN AND REGISTRATION FORMS

---
+++ ethics/register.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1613112
+ * Date: 12/04/2017
+ * Time: 09:54
+ */
+
+?>
+<!DOCTYPE html>
+
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+                <form>
+                    <input type="text" name="firstname" placeholder="firstname"/>
+                    <input type="text" name="lastname" placeholder="lastname"/>
+                    <input type="text" name="username" placeholder="firstname"/>
+                    <input type="text" name="userid" placeholder="login ID"/>
+                    <input type="email" name="email" placeholder="Email"/>
+                    <input type="text" name="address" placeholder="Address Line 1"/>
+                    <input type="text" name="address" placeholder="Address Line 2"/>
+                    <input type="text" name="address" placeholder="Address Line 3"/>
+                    <select name="role">
+                        <option value="nothing">Please select a role</option>
+                        <option value = "STUDENT">Student</option>
+                        <option value="EAO">EAO</option>
+                        <option value="ADMIN">Admin</option>
+                    </select>
+                    <input type="password" name="password" placeholder="Password"/>
+                    <button> Log in</button>
+                </form>
+
+
+    </body>
+    </html>
+
