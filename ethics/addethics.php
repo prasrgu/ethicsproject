d40b6a560f5b75e55d2f8ce66b76ff362f73a943
prasrgu <prasrgu@hotmail.com>
2017-04-19 23:33:17 +0100

    testing the project details

--- ethics/addethics.php
+++ ethics/addethics.php
@@ -2,10 +2,10 @@
 session_start();
 include('connection.php');
 
-if(isset($_SESSION['ufname'])) {
- if($_SERVER['REQUEST_METHOD']=='POST'){
+if (isset($_SESSION['ufname'])) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        if(!empty($_POST['ptitle'])&&!empty($_POST['pdesc'])&&!empty($_POST['psubdate'])) {
+        if (!empty($_POST['ptitle']) && !empty($_POST['pdesc']) && !empty($_POST['psubdate'])) {
 
             $curdate = date("d-m-Y");
             $ptitle = $_POST['ptitle'];
@@ -19,20 +19,12 @@ if(isset($_SESSION['ufname'])) {
 
             $sq1 = "INSERT INTO projects (title, description, submissionDate, std_ID)  VALUES ('$ptitle','$pdesc','$psubdate', '$stdId')";
 
-            if (mysqli_query($link, $sq1)) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sq1 . "<br>" . mysqli_error($link);
-            }
 
             mysqli_close($link);
         }
-        }
-  }
-
-
-else{
-    header ('location : index.php');
+    }
+} else {
+    header('location : index.php');
 }
 
 
@@ -41,21 +33,20 @@ else{
 <html>
 <head>
     <title>Add Project</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Philosopher" rel="stylesheet">
-    <link rel="stylesheet" href="style.css" >
+    <link rel="stylesheet" href="style.css">
 
     <script src="https://use.fontawesome.com/425f6058af.js"></script>
-
 
 
 </head>
@@ -64,7 +55,8 @@ else{
     <div class="container-fluid wraps">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -79,7 +71,8 @@ else{
                 <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
                 <li><a href="#">Profile</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Manage Projects <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">Manage Projects <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">Add Project</a></li>
                         <li role="separator" class="divider"></li>
@@ -101,39 +94,39 @@ else{
 </nav>
 
 <h2 class="col-md-4 col-md-offset-4 down">Add Ethics</h2> <br/>
-    <div>
-        <span>Project Title: </span> <?php echo $ptitle; ?>
-        <span>Submission Date: </span> <?php echo $psubdate; ?>
-    </div>
+<div>
+    <span>Project Title: </span> <?php echo $ptitle; ?>
+    <span>Submission Date: </span> <?php echo $psubdate; ?>
+</div>
 
 <form class="form-horizontal" action="success.php" method="post">
     <div class="form-group">
         <label for="ptitle" class="col-md-2 col-md-offset-2 control-label">Title</label>
         <div class="col-md-4">
-            <input type="text" name="ptitle"  class="form-control" placeholder="Project Title"/>
+            <input type="text" name="ptitle" class="form-control" placeholder="Project Title"/>
         </div>
     </div>
     <div class="form-group">
         <label for="pdesc" class="col-md-2 col-md-offset-2 control-label">Description</label>
-        <div class="col-md-4" >
-            <textarea class="form-control" placeholder="Please Enter The Description" ></textarea>
-        </div >
+        <div class="col-md-4">
+            <textarea class="form-control" placeholder="Please Enter The Description"></textarea>
+        </div>
     </div>
     <div class="form-group">
-        <label for="subdate" class="col-md-2 col-md-offset-2 control-label" >Submission Date</label>
+        <label for="subdate" class="col-md-2 col-md-offset-2 control-label">Submission Date</label>
         <div class="col-md-4">
-            <input type="date" name="psubdate"  class="form-control"/>
+            <input type="date" name="psubdate" class="form-control"/>
         </div>
     </div>
     <div class="form-group">
         <label for="etfile" class="col-md-2 col-md-offset-2 control-label">Ethics Document</label>
         <div class="col-md-4">
-            <input type="file" name="etdoc"  class="form-control" />
+            <input type="file" name="etdoc" class="form-control"/>
         </div>
 
     </div>
     <button>Add Suppporting Documents</button>
-    <button type="submit"  class = "btn btn-primary ">Add Ethics</button>
+    <button type="submit" class="btn btn-primary ">Add Ethics</button>
 
 </form>
 </body>
