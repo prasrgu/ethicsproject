2bb03ad63f52820cd145292a0538f26ba184762d
prasrgu <prasrgu@hotmail.com>
2017-04-19 15:39:35 +0100

    trying to see the user in session

--- ethics/landing.php
+++ ethics/landing.php
@@ -89,14 +89,14 @@ if(isset($_SESSION['ufname'])) {
     <div class="container" >
         <div class="row">
             <div class = "col-md-4 col-md-offset-4">
-                <span class="cent"> Add Project <i class="fa fa-plus fa-5x"  aria-hidden="true"></i></span>
-                <span class="cent">Edit Project <i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i></span>
+                <span class="cent"> <a>Add Project <i class="fa fa-plus fa-5x"  aria-hidden="true"></i></a></span>
+                <span class="cent"><a>Edit Project <i class="fa fa-pencil-square-o fa-5x" aria-hidden="true"></i></a></span>
 
 
             </div>
             <div class = "col-md-4 col-md-offset-4">
-                <span class="cent"> View Projects <i class="fa fa-list fa-5x" aria-hidden="true" ></i></span>
-                <span class="cent">Edit Profile <i class="fa fa-wrench fa-5x" aria-hidden="true"></i></span>
+                <span class="cent"> <a>View Projects <i class="fa fa-list fa-5x" aria-hidden="true" ></i></a></span>
+                <span class="cent"><a>Edit Profile <i class="fa fa-wrench fa-5x" aria-hidden="true"></i></a></span>
 
 
             </div>
