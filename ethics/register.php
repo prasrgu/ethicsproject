f706961d7d9425ce67dfa2410685e3e5ddc7b938
prasrgu <prasrgu@hotmail.com>
2017-04-20 23:30:05 +0100

    logout implementation

--- ethics/register.php
+++ ethics/register.php
@@ -1,7 +1,7 @@
 <?php
     include ('connection.php');
     if($_SERVER['REQUEST_METHOD']==='POST'){
-        if(empty($_POST['firstname']) || empty($_POST['lastname'])||empty($_POST['userid'])||empty($_POST['email'])||($_POST['role'] =='nothing')||empty($_POST['password'])||empty($_POST['password1'])){
+        if(empty($_POST['firstname']) || empty($_POST['lastname'])||empty($_POST['userid'])||empty($_POST['email'])||($_POST['role'] =='nothing')||empty($_POST['password'])||empty($_POST['password1'])||strlen($_POST['userid'])!=8 ){
             echo "Please fill in all required fields";
         }
 
