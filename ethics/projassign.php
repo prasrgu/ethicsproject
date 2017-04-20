1b3e97a5eaf591f4757d16fc67efba388660e869
prasrgu <prasrgu@hotmail.com>
2017-04-20 22:35:30 +0100

    logout implementation

--- ethics/projassign.php
+++ ethics/projassign.php
@@ -3,10 +3,12 @@
  * Created by PhpStorm.
  * User: AP1
  * Date: 20/04/2017
- * Time: 16:38
+ * Time: 17:36
  */
 
 include('connection.php');
+$proj_ID=$_GET['p'];
+$std_ID = $_GET['sid'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,22 +32,13 @@ include('connection.php');
 
 </head>
 <body>
-<?php
-        $sqqq ="SELECT * FROM projects ";
-       $answe= mysqli_query($link, $sqqq);
-        if(mysqli_num_rows($answe)>0){
-            echo "<table class='table table-striped'><thead><tr><td>Student Name</td><td>Student ID</td> <td>Project Title</td></tr></thead><tbody>";
-        while($resu=mysqli_fetch_assoc($answe)) {
-            $dse = "SELECT * FROM  student WHERE student_ID='{$resu['std_ID']}'";
-                    $fds=mysqli_query($link, $dse);
-                   $svd= mysqli_fetch_assoc($fds);
-            echo "<tr><td>" . "<a href='projassign.php?p={$resu['id']}&sid={$resu['std_ID']}'>". $svd['firstname'] . "  ".$svd['lastname'] . "</a>". "</td><td>" . $svd['student_ID'] . "</td><td>" . $resu['title'] . "</td></tr>";
-        }
-        echo "</tbody></table>";
-        }
-        else{
-            echo "There are no student projects at the moment. Please check back later";
-        }
+        <?php
+                echo "Hello World";
+
+
         ?>
+
+
+
 </body>
 </html>
