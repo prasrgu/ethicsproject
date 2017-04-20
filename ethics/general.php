bb754ba9329bbb79275b2e3ad86a115d9bae4481
prasrgu <prasrgu@hotmail.com>
2017-04-20 16:17:38 +0100

    testing the project details

--- ethics/general.php
+++ ethics/general.php
@@ -51,13 +51,13 @@
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
-                <li><span onclick="<?php
+                <li><a onclick="<?php
 
 
                     unset($_SESSION);
                     session_destroy();
                     header('location:index.php');
-                    ?>">Logout</span></li>
+                    ?>">Logout</a></li>
 
             </ul>
         </div><!-- /.navbar-collapse -->
