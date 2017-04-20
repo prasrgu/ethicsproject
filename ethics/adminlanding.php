4472b61fec5a12c532bc6afce6b1606eadfaaeca
prasrgu <prasrgu@hotmail.com>
2017-04-20 17:33:54 +0100

    logout implementation

--- ethics/adminlanding.php
+++ ethics/adminlanding.php
@@ -7,7 +7,30 @@
  */
 
 include('connection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Philosopher" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" >
+
+    <script src="https://use.fontawesome.com/425f6058af.js"></script>
+
+
+
+</head>
+<body>
+<?php
         $sqqq ="SELECT * FROM projects ";
        $answe= mysqli_query($link, $sqqq);
         if(mysqli_num_rows($answe)>0){
@@ -23,3 +46,6 @@ include('connection.php');
         else{
             echo "There are no student projects at the moment. Please check back later";
         }
+        ?>
+</body>
+</html>
