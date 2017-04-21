9a274564a71098b23fd77191ed5c98a6ea430995
prasrgu <prasrgu@hotmail.com>
2017-04-21 04:40:51 +0100

    logout implementation

--- ethics/eaolanding.php
+++ ethics/eaolanding.php
@@ -5,4 +5,45 @@
  * Date: 20/04/2017
  * Time: 17:21
  */
-echo "Hello World";
+include ('connection.php');
+session_start();
+if(isset($_SESSION['uid'])) {
+    $sos = "SELECT * FROM projects WHERE id IN (SELECT projID FROM staff_proj WHERE staffId='{$_SESSION['uid']}')";
+
+        $sdvsd=mysqli_query($link, $sos);
+        if(mysqli_num_rows($sdvsd)>0){?>
+            <table>
+                <thead>
+                    <tr>
+                        <th> Project Title</th>
+                        <th> Student Name</th>
+                        <th>Submission Deadline</th>
+
+                    </tr>
+
+                </thead>
+                <tbody>
+                <?php while($sdsd=mysqli_fetch_assoc($sdvsd)) {
+                        $cvbd = "SELECT firstname, lastname FROM student WHERE student_ID = '{$sdsd['std_ID']}'"
+                        $ed = mysqli_query($link, $cvbd);
+                        $sva= mysqli_fetch_assoc($ed);
+                    ?>
+                    <tr >
+                    <td ><? echo $sdsd['title'];?> </td >
+                    <td ><? echo $sva['firstname']. "  ".$sva['firstname'];?>  </td >
+                    <td ><? echo $sdsd['submissionDate'];?></td >
+
+                </tr >
+               <? }
+
+                ?>
+                </tbody>
+            </table>
+      <?  }
+        else{
+            ?>
+            <h3> You Currently Have no projects Allocated to you at the Moment. Please Check Back Later </h3>
+        <?php
+        }
+
+            }
