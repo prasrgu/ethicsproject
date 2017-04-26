6a7bef123aa7887db4f2ac4ef31d74ccfd5b04f4
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-26 16:14:20 +0100

    final indentation

--- ethics/apiendpoint.php
+++ ethics/apiendpoint.php
@@ -65,7 +65,7 @@ $req = $_GET['que'];
 function indent($json)
 {
 
-    $result = '';
+    $quo = '';
     $pos = 0;
     $strLen = strlen($json);
     $indentStr = '  ';
@@ -75,42 +75,41 @@ function indent($json)
 
     for ($i = 0; $i <= $strLen; $i++) {
 
-        // Grab the next character in the string.
+
         $char = substr($json, $i, 1);
 
-        // Are we inside a quoted string?
+
         if ($char == '"' && $prevChar != '\\') {
             $outOfQuotes = !$outOfQuotes;
 
-            // If this character is the end of an element,
-            // output a new line and indent the next line.
+
+
         } else if (($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
+            $quo .= $newLine;
             $pos--;
             for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
+                $quo .= $indentStr;
             }
         }
 
-        // Add the character to the result string.
-        $result .= $char;
 
-        // If the last character was the beginning of an element,
-        // output a new line and indent the next line.
+        $quo .= $char;
+
+
         if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
+            $quo .= $newLine;
             if ($char == '{' || $char == '[') {
                 $pos++;
             }
 
             for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
+                $quo .= $indentStr;
             }
         }
 
         $prevChar = $char;
     }
 
-    return $result;
+    return $quo;
 }
 
