c7a9c71ccc1d209837d2380adbab5ae7dd2ab815
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-26 16:37:07 +0100

    final indentation

--- ethics/apiendpoint.php
+++ ethics/apiendpoint.php
@@ -27,7 +27,7 @@ $req = $_GET['que'];
 
                 }
 
-            }elseif(($req = "students")  ){
+            }elseif(($req == "students")  ){
                 $query = "SELECT firstname, lastname, email, address, student_ID FROM  student";
                 $result = mysqli_query($link, $query);
                 if(mysqli_num_rows($result)>0) {
@@ -47,7 +47,7 @@ $req = $_GET['que'];
 
                 }
 
-            }elseif(($req = "staff")){
+            }elseif(($req == "staff")){
                 $query = "SELECT firstname, lastname, email,role, address, staff_ID FROM  staff";
                 $result = mysqli_query($link, $query);
                 if(mysqli_num_rows($result)>0) {
