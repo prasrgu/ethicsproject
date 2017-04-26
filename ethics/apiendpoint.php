c4596679cb8d7e62ead32e3d70e3692bc06494b7
1613112 <p.tontalapur@rgu.ac.uk>
2017-04-26 13:09:46 +0100

    now dealing with landing page hashing

---
+++ ethics/apiendpoint.php
@@ -0,0 +1,45 @@
+<?php
+
+
+$req = $_GET['que'];
+
+        header('Content-Type: application/json');
+        include('connection.php');
+
+        if (!empty($req)){
+            if(($req=="projects") ||($req=="Projects") || ($req=="PROJECTS")) {
+                $query = "SELECT * FROM  projects";
+                $result = mysqli_query($link, $query);
+                if(mysqli_num_rows($results)>0) {
+                 $res=  mysqli_fetch_assoc($result);
+                        $pry=json_encode($res);
+                    echo $pry;
+                }else{
+
+                }
+
+            }elseif(($req = "students") ||($req=="Students")|| ($req=="STUDENTS") ){
+                $query = "SELECT * FROM  student";
+                $result = mysqli_query($link, $query);
+                if(mysqli_num_rows($results)>0) {
+
+                    json_encode(array('text' => 'omrele'));
+                }else{
+
+                }
+
+            }elseif(($req = "staff") ||($req=="Staff")|| ($req=="STAFF")){
+                $query = "SELECT * FROM  staff";
+                $result = mysqli_query($link, $query);
+                if(mysqli_num_rows($results)>0) {
+
+                    json_encode(array('text' => 'omrele'));
+                }else{
+
+                }
+
+            }
+
+        }else{
+
+        }
