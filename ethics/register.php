252e0673fc7b4f4232f965d7ea0ee45aa76f9aa8
prasrgu <prasrgu@hotmail.com>
2017-05-01 22:32:48 +0100

    updated server

--- ethics/register.php
+++ ethics/register.php
@@ -7,11 +7,11 @@
     $dbname = "localdb";
 
 $sql = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($sql);
+$result = mysqli_query($link, $sql);
 
 if (!$result) {
     echo "DB Error, could not list tables\n";
-    echo 'MySQL Error: ' . mysql_error();
+    echo 'MySQL Error: ' . mysqli_error($link);
     exit;
 }
 
