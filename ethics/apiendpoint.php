2eda11eac9ab71611755c619691d93f40657a19e
prasrgu <prasrgu@hotmail.com>
2017-05-01 17:47:03 +0100

    updated server nmdsf

--- ethics/apiendpoint.php
+++ ethics/apiendpoint.php
@@ -1,6 +1,8 @@
 <?php
 
 $request_verb = $_SERVER['REQUEST_METHOD'];
+print_r($_SERVER);
+exit;
 $requ = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 $requ = array_map('strtolower', $requ);
 echo "project is";
